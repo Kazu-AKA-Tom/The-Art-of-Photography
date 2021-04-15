ddda11ca6f68b2935622248068a7e8a4a290d4b8
Kazu_AKA_Tom <tomeoliveirareis03@gmail.com>
2021-04-15 10:27:49 +0100

    New code incerted, hope to make it work

---
+++ Javascript/webflow.js
@@ -0,0 +1,13 @@
+$('.navTrigger').click(function () {
+
+  $(this).toggleClass('active');
+
+  console.log("Clicked menu");
+
+  $("#mainListDiv").toggleClass("show_list");
+  
+  $("#mainListDiv").fadeIn();
+
+});
+
+
